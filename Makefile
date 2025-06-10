ffd645f84345a5c4e9401dfaae9b5200d689b8d7
colonelkarani <quavix0000@gmail.com>
2025-06-11 00:26:51 +0300

    getting the hang of it

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ CFLAGS=-Wall -g
 
 clean:
 	del /F ex1.exe
+	del /F ex3.exe
+
---
+++ ex3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int age = 23;
+    int height = 66;
+
+    printf( "My name is victor and i am %d years old\n" ,age);
+    printf("My name is victor and i am %d inches tall\t", height);
+    printf("Beep\a");
+
+
+    return 0;
+}
